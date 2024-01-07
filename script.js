02e1a6e2ec8cf828f28bb06c3eb63981b80f0289
PoKPoK68 <141999747+PoKPoK68@users.noreply.github.com>
2024-01-07 14:48:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,84 @@
-const searchInput = document.querySelector('.search');
-const listItems = document.querySelectorAll('.list li');
-const resultImagesContainer = document.getElementById('resultImagesContainer');
+const searchInput = document.querySelector(".search");
+const listItems = document.querySelectorAll(".list li");
+const resultImagesContainer = document.getElementById("resultImagesContainer");
 
 // Correspondance entre les éléments de la liste, les chemins des images et les légendes
 const imagePaths = {
-  'Thrustmaster T248': {
-    paths: ['images/T248_profiler.jpg', 'images/T248_AMS2.jpg', 'images/T248_RF2.jpg', 'images/T248_ACC.jpg'], //chemin des images dans ce tableau
-    legends: ['Volant', 'Automobilista 2', 'rFactor 2', 'Assetto Corsa Competizione'] // Légende de chaque image dans ce tableau
+  "Thrustmaster T248": {
+    paths: [
+      "images/T248_profiler.jpg",
+      "images/T248_AMS2.jpg",
+      "images/T248_RF2.jpg",
+      "images/T248_ACC.jpg",
+    ], //chemin des images dans ce tableau
+    legends: [
+      "Volant",
+      "Automobilista 2",
+      "rFactor 2",
+      "Assetto Corsa Competizione",
+    ], // Légende de chaque image dans ce tableau
   },
-  'Simagic Alpha Ultimate': {
-    paths: ['images/Simagic_alpha_ultimate.jpg', 'images/Simagic_alpha_ultimate.jpg'],
-    legends: ['Légende 3', 'Légende 4']
+  "Simagic Alpha Ultimate": {
+    paths: [
+      "images/Simagic_Alpha_Ultimate_AMS2.jpg",
+      "images/Simagic_alpha_ultimate.jpg",
+    ],
+    legends: ["Automobilista 2", "Légende 4"],
   },
-  'Fanatec CSL DD': {
-    paths: ['chemin/vers/images/image3_1.jpg', 'chemin/vers/images/image3_2.jpg'],
-    legends: ['Légende 5', 'Légende 6']
+  "Fanatec CSL DD": {
+    paths: [
+      "chemin/vers/images/image3_1.jpg",
+      "chemin/vers/images/image3_2.jpg",
+    ],
+    legends: ["Légende 5", "Légende 6"],
   },
-  'Logitech G29 / G920': {
-    paths: ['chemin/vers/images/image3_1.jpg', 'chemin/vers/images/image3_2.jpg'],
-    legends: ['Légende 7', 'Légende 8']
+  "Logitech G29 / G920": {
+    paths: [
+      "chemin/vers/images/image3_1.jpg",
+      "chemin/vers/images/image3_2.jpg",
+    ],
+    legends: ["Légende 7", "Légende 8"],
   },
   // Ajoutez d'autres éléments au besoin
 };
 
 // Gestionnaire d'événements pour la recherche
-searchInput.addEventListener('input', function () {
+searchInput.addEventListener("input", function () {
   const searchValue = this.value.toLowerCase();
-  
-  listItems.forEach(item => {
+
+  listItems.forEach((item) => {
     const itemText = item.textContent.toLowerCase();
     const isMatch = itemText.includes(searchValue);
 
     // Utilisation d'une classe pour gérer la visibilité des éléments
     if (isMatch) {
-      item.classList.remove('hidden');
+      item.classList.remove("hidden");
     } else {
-      item.classList.add('hidden');
+      item.classList.add("hidden");
     }
   });
 });
 
 // Gestionnaire d'événements pour la sélection d'un élément de la liste
-listItems.forEach(item => {
-  item.addEventListener('click', function () {
+listItems.forEach((item) => {
+  item.addEventListener("click", function () {
     // Récupérez le chemin de l'image et la légende à partir de la correspondance
     const imageData = imagePaths[this.textContent];
 
     // Effacez le contenu précédent du conteneur d'images
-    resultImagesContainer.innerHTML = '';
+    resultImagesContainer.innerHTML = "";
 
     // Affichez chaque image dans le conteneur avec sa légende
     imageData.paths.forEach((path, index) => {
-      const figureElement = document.createElement('figure');
-      const figcaptionElement = document.createElement('figcaption');
-      const imageElement = document.createElement('img');
+      const figureElement = document.createElement("figure");
+      const figcaptionElement = document.createElement("figcaption");
+      const imageElement = document.createElement("img");
 
       // Ajoutez une classe pour permettre le placement côte à côte
-      figureElement.classList.add('image-container');
+      figureElement.classList.add("image-container");
 
       imageElement.src = path;
-      figcaptionElement.textContent = imageData.legends[index] || '';
+      figcaptionElement.textContent = imageData.legends[index] || "";
 
       figureElement.appendChild(figcaptionElement);
       figureElement.appendChild(imageElement);
@@ -68,7 +87,7 @@ listItems.forEach(item => {
     });
 
     // Retirez la classe 'hidden' pour afficher le conteneur d'images
-    resultImagesContainer.classList.remove('hidden');
+    resultImagesContainer.classList.remove("hidden");
 
     // Réinitialisez la barre de recherche lors du clic sur un volant
     // searchInput.value = '';
